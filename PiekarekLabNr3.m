32a47bc2a278d24e336d57e5228120b3e50a3b7d
Yetisio <jakub.piekarek@wp.pl>
2023-05-07 15:46:47 +0200

    zmiany 06.05

--- PiekarekLabNr3.m
+++ PiekarekLabNr3.m
@@ -1,9 +1,10 @@
 %Jakub Piekarek
-TzewN=-20;
-TwewN=20;
-TpN=15;
-PgN=20000;
-Vw=2.5*2*2;
+%wartosci nominalne
+TzewN=-20;  % Celcius
+TwewN=20;   % Celcius
+TpN=15;     % Celcius
+PgN=20000;  % WAT
+Vw=2.5*2*2; %indeks 264202 
 Vp=Vw/2;
 cpp=1000; 
 rop=1.2;
@@ -12,33 +13,37 @@ rop=1.2;
 %K1,Kp,Kd,fPN;
 %cvw,cvp
 
-Cvv=cpp*rop*Vw;
-Cvp=cpp*rop*Vp;
 %zalozenia zadania
+%Identyfikacja
 fPN = Vp / (24*60*60 / 4 ); %wymiana powietrza 4 razy na dobe
-
 Kd = (PgN-cpp*rop*fPN*(TwewN-TzewN))/(2*(TwewN-TzewN) +7*(TwewN-TpN));
 K1 = 2*Kd; % 2 razy wieksza material konsturkcja
-
 Kp=(Kd*(TpN-TzewN))/(TwewN-TpN);
 
+%parametry dynamiczne
+Cvv=cpp*rop*Vw;
+Cvp=cpp*rop*Vp;
 
-%Twew0= TwewN;
-%Tp0=TpN;
+%paramtery warunkow poczatkowych
 Tzew0=TzewN;
 Fp0 = fPN;
 Pg0=PgN;
-
 %liczenie lab3
-Tp0= (PgN+(Kd*K1*Tzew0)+(K1+Kp+Tzew0)+(cpp*rop*Fp0*Kd*Tzew0)+(cpp*rop*Fp0*Kp*Tzew0)+(Kd*Kp*Tzew0))/(K1+Kd+(cpp*rop*Fp0*Kd)-(Kd*Kp));
-Twew0=Kd/Kp*(Tp0-Tzew0)+Tp0;
+%stan równowagi
+Twew0=Tzew0+(Pg0/(K1+(cpp*rop*Fp0)+(Kp*Kd)/(Kp+Kd)));
+Tp0=(Kp*Twew0+Kd*Tzew0)/(Kp+Kd);
+%Twew0= TwewN;
+%Tp0=TpN;
 
+%zaklocenie
 czas=50000;
-%stoptime zamiast 10 50000
 czas_skok=5000;
 dTzew=0;
 dPg=0;
 dFp=0;
+
+%przypisanie do zmiennej symulacji
 [t]=sim('PiekarekLabNr3_si', czas);
+%wyswietlanie wykresow
 figure,plot(t.tout,t.Twew);
 figure,plot(t.tout,t.Tp);
